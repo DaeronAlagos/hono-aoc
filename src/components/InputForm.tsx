bb7ddd11bfe20954556db4160fe6ef2f42736a5d
James Curtis <mybastion@gmail.com>
2024-12-03 08:53:05 +0000

    feat(project): Refine styling on input  form

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,14 +1,14 @@
 export const InputForm = ({ year, day }: { year: string, day: string }) => {
     return (
         <form action={`/${year}/${day}`} method="post" className="max-w-2xl mx-auto px-4">
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4 mt-16">
                 <textarea 
                     name="input" 
                     className="h-48 p-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 ></textarea>
                 <button 
                     type="submit" 
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full sm:w-auto"
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full sm:w-auto"
                 >
                     Submit
                 </button>
