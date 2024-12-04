5affe2a9e451cc5ace729fa070eca6a92976ebdc
James Curtis <mybastion@gmail.com>
2024-12-04 08:31:42 +0000

    feat(project): Remove shadow from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import { ChallengeList } from "./ChallengeList"
 
 export const Header = () => {
     return (
-        <header className="w-full shadow-lg">
+        <header className="w-full">
             <div className="mx-auto max-w-7xl text-center py-6 px-4 bg-[repeating-linear-gradient(45deg,#166534,#166534_10px,#15803d_10px,#15803d_20px)] bg-gradient-to-r from-red-600/90 to-green-700/90">
-                <h1 className="text-4xl font-bold text-white drop-shadow-lg">
+                <h1 className="text-4xl font-bold text-white">
                     🎄 Advent of Code with Hono ⭐
                 </h1>
             </div>
