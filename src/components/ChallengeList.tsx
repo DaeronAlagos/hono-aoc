6f7b343d4ca2e3bf3d869007b1f6fab9c5d5a1c8
James Curtis <mybastion@gmail.com>
2024-12-04 08:32:36 +0000

    feat(project): Update challenge list to include titles

--- src/components/ChallengeList.tsx
+++ src/components/ChallengeList.tsx
@@ -6,29 +6,51 @@ interface Challenge {
 const challenges = [
   {
     year: 2024,
-    days: [1, 2, 3]
+    days: [
+      {
+        number: 1,
+        title: 'Historian Hysteria'
+      },
+      {
+        number: 2,
+        title: 'Red-Nosed Reports'
+      },
+      {
+        number: 3,
+        title: 'Mull It Over'
+      },
+      {
+        number: 4,
+        title: 'Ceres Search'
+      },
+    ]
   },
   {
     year: 2023,
-    days: [1]
+    days: [
+      {
+        number: 1,
+        title: 'Reindeer Olympics'
+      }
+    ]
   }
 ]
 
 export const ChallengeList = () => {
   return (
-    <div className="p-4">
+    <div className="mx-auto max-w-7xl py-4">
       <div className="space-y-4">
         {challenges.map((challenge) => (
           <div key={challenge.year} className="border rounded-lg p-4">
             <h3 className="text-xl font-semibold mb-2">Year {challenge.year}</h3>
-            <div className="grid grid-cols-4 gap-2">
+            <div className="flex gap-2">
               {challenge.days.map((day) => (
                 <a 
-                  href={`/${challenge.year}/${day}`}
-                  key={`${challenge.year}-${day}`}
-                  className="p-1 rounded hover:bg-gray-200 cursor-pointer text-center"
+                  href={`/${challenge.year}/${day.number}`}
+                  key={`${challenge.year}-${day.number}`}
+                  className="py-2 px-4 rounded bg-gray-100 hover:bg-gray-200 cursor-pointer text-center"
                 >
-                  {day}
+                  {day.number}
                 </a>
               ))}
             </div>
